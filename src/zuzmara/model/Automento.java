9143c1087b17a1f7fa93fa2556eda2643468885d
BAdam6 <bokor.adam.hu@gmail.com>
2026-03-28 18:59:31 +0100

    Add AutoAllapot import and modify Automento methods

--- src/zuzmara/model/Automento.java
+++ src/zuzmara/model/Automento.java
@@ -1,4 +1,19 @@
 package zuzmara.model;
+import zuzmara.enums.AutoAllapot;
+
 public class Automento {
-    
+
+    public void elszallit(Auto a) {
+        Skeleton.nyit("Automento.elszallit(Auto)");
+
+        a.allapotValtoztat(AutoAllapot.VARAKOZIK);
+
+        Skeleton.zar("Automento.elszallit() visszater");
+    }
+
+    public void mentesRegisztral(Auto a) {
+        Skeleton.nyit("Automento.mentesRegisztral(Auto)");
+        Skeleton.zar("Automento.mentesRegisztral() visszater");
+    }
 }
+
