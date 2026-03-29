372dc8557fe16bdb4557b788aeacff7bf45ceba3
BAdam6 <bokor.adam.hu@gmail.com>
2026-03-29 15:49:37 +0200

    Enhance Automento class with Javadoc comments
    Added documentation for the Automento class and its methods.

--- src/zuzmara/model/Automento.java
+++ src/zuzmara/model/Automento.java
@@ -1,8 +1,22 @@
 package zuzmara.model;
+
 import zuzmara.enums.AutoAllapot;
 
+/**
+ * Az Automento osztály az autók elszállításáért felelős.
+ * Feladata a mentésre váró autók kezelése, illetve azok állapotának módosítása.
+ * 
+ * A működés során a Skeleton osztály segítségével naplózza a metódushívásokat.
+ */
 public class Automento {
 
+    /**
+     * Els szállít egy autót.
+     * Az autó állapotát VÁRAKOZIK állapotra állítja,
+     * jelezve, hogy a mentés sikeresen megtörtént.
+     *
+     * @param a az elszállítandó autó
+     */
     public void elszallit(Auto a) {
         Skeleton.nyit("Automento.elszallit(Auto)");
 
@@ -11,9 +25,14 @@ public class Automento {
         Skeleton.zar("Automento.elszallit() visszater");
     }
 
+    /**
+     * Regisztrál egy mentésre váró autót.
+     * Skeleton szinten jelenleg csak naplózás történik.
+     *
+     * @param a a mentésre váró autó
+     */
     public void mentesRegisztral(Auto a) {
         Skeleton.nyit("Automento.mentesRegisztral(Auto)");
         Skeleton.zar("Automento.mentesRegisztral() visszater");
     }
 }
-
