96347320413fb2037b5b1de0c2b45b2e603712b6
BAdam6 <bokor.adam.hu@gmail.com>
2026-04-29 16:21:31 +0000

    Create ILepheto.java

---
+++ Prototipus/src/ILepheto.java
@@ -0,0 +1,11 @@
+/**
+ * Az ILepheto interfész azokat az objektumokat jelöli,
+ * amelyek az idő múlására reagálnak.
+ */
+public interface ILepheto {
+
+    /**
+     * Egy játékbeli időegység elteltének kezelése.
+     */
+    void idoEltelt();
+}
