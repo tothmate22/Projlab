ff529dac697b5d00b5e9e7b47721947711b9d04a
BAdam6 <bokor.adam.hu@gmail.com>
2026-04-30 17:21:53 +0200

    Create IInfo.java

---
+++ Prototipus/src/interfaces/IInfo.java
@@ -0,0 +1,11 @@
+/**
+ * Az IInfo interfész azokat az objektumokat jelöli,
+ * amelyekről információ kérdezhető le a cmd-ből
+ */
+public interface IInfo {
+
+    /**
+     * Lekérdezi egy objektumról tárolt információkat, majd kiírja a konzolra
+     */
+    void getInfo(){};
+}
